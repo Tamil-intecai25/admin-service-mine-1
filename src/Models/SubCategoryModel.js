14c5fd13bda0a98ed4921877d58d59ad21edc3c0
Tamil-intecai25 <tamil@intecai.in>
2025-03-28 11:49:27 +0530

    upadate the socket io connection

--- src/Models/SubCategoryModel.js
+++ src/Models/SubCategoryModel.js
@@ -1,47 +1,22 @@
+const { number } = require("keygenerator/lib/keygen");
+
 const AdminConnection = require("../Database/Connection").getAdminDB();
 
 // function Schema() {
 let subCategorySchema = new AdminConnection.Schema(
   {
     subCategoryId: { type: String, required: true, unique: true },
-    sellerId: { type: String,},
-    categoryId: { type: String,},
-    dishName: {
-      type: String,
-    //   required: true,
-    },
-    dishType: {
-      type: String,
-    //   required: true,
-    },
-    dishImage: {
-      type: String,
-    //   required: true,
-    },
-    servingSize: {
-      type: String,
-    //   required: true,
-    },
-    description: {
-      type: String,
-    //   required: true,
-    },
-    dishPrice: {
-      type: String,
-    //   required: true,
-    },
-    gst: {
-      type: String,
-    //   required: true,
-    },
-    packagePrice: {
-      type: String,
-    //   required: true,
-    },
-    finalDishPrice: {
-      type: String,
-    //   required: true,
-    },
+    sellerId: { type: String },
+    categoryId: { type: String },
+    dishName: { type: String },
+    dishType: { type: String },
+    dishImage: { type: String },
+    servingSize: { type: String },
+    description: { type: String },
+    dishPrice: { type: String },
+    gst: { type: String },
+    packagePrice: { type: String },
+    finalDishPrice: { type: String },
     approvalStatus: {
       type: String,
       enum: ["active", "inactive"],
